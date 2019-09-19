fbf816563098a46f2428db252eb48dcc91b1340a
Juan Manuel Incaurgarat <juanofx@gmail.com>
2019-09-19 21:50:48 +0200

    wip

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,12 +1,23 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   res.render('index');
-}
+};
 
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
-}
+};
 
-exports.createStore = (req, res) => {
+exports.createStore = async (req, res) => {
   console.log(req.body);
-  res.json(req.body);
-}
+  const store = await new Store(req.body).save();
+  req.flash('success', `Added ${store.name}`);
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  console.log(stores);
+  res.render('stores', { title: 'Stores', stores });
+};
