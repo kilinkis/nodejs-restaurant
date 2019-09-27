6ec067905e7a9404fb23342e3f19897b9e931f14
Juan Manuel Incaurgarat <juanofx@gmail.com>
2019-09-27 15:49:35 +0200

    Added reviews

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -88,7 +88,8 @@ exports.updateStore = async (req, res) => {
 };
 
 exports.getStoreBySlug = async (req, res, next) => {
-  const store = await Store.findOne({ slug: req.params.slug }); // .populate('author) would also include the data from the store owner
+  const store = await Store.findOne({ slug: req.params.slug })
+    .populate('author reviews');
   if (!store) return next();
   res.render('store', { store, title: store.name });
 };
@@ -159,4 +160,9 @@ exports.getHearts = async (req, res) => {
     _id: { $in: req.user.hearts }
   });
   res.render('stores', {title: 'Hearted Stores', stores})
+}
+
+exports.getTopStores = async (req, res) => {
+  const stores = await Store.getTopStores();
+  res.render('topStores', { stores, title: 'Top Stores!'});
 }
--- models/Store.js
+++ models/Store.js
@@ -40,6 +40,9 @@ const storeSchema = new mongoose.Schema({
     ref: 'User',
     required: 'You must provide an author'
   }
+}, {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
 });
 
 // Define indexes
@@ -73,5 +76,41 @@ storeSchema.statics.getTagsList = function() {
     { $sort: { count: -1 } }
   ]);
 };
+
+storeSchema.statics.getTopStores = function() {
+  return this.aggregate([
+    // Lookup Stores and populate their reviews
+    { $lookup: { from: 'reviews', localField: '_id', foreignField: 'store', as: 'reviews' }},
+    // filter for only items that have 2 or more reviews
+    { $match: { 'reviews.1': { $exists: true } } },
+    // Add the average reviews field
+    { $project: {
+      photo: '$$ROOT.photo',
+      name: '$$ROOT.name',
+      reviews: '$$ROOT.reviews',
+      slug: '$$ROOT.slug',
+      averageRating: { $avg: '$reviews.rating' }
+    } },
+    // sort it by our new field, highest reviews first
+    { $sort: { averageRating: -1 }},
+    // limit to at most 10
+    { $limit: 10 }
+  ]);
+}
+
+// find reviews where the stores _id property === reviews store property
+storeSchema.virtual('reviews', {
+  ref: 'Review', // what model to link to
+  localField: '_id', // which field on the store?
+  foreignField: 'store' // which field in the review?
+});
+
+function autopopulate(next) {
+  this.populate('reviews');
+  next();
+}
+
+storeSchema.pre('find', autopopulate);
+storeSchema.pre('findOne', autopopulate);
 
 module.exports = mongoose.model('Store', storeSchema);
